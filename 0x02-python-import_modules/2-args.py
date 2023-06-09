536b9c83bee8fa721c035ffd45172f9163c0d8bf
edgaryungo <eddy8471@gmail.com>
2023-06-09 00:56:54 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    i = len(sys.argv)
+    i = len(sys.argv) - 1
 
     if i == 0:
         print("{} arguments.".format(i))
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         print("{} argument:".format(i))
     else:
         print("{} arguments:".format(i))
-
+    if i >= 1:
         for index, arg in enumerate(sys.argv, start=1):
                 print("{}: {}".format(index, arg))
