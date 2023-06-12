0f83b2a35d0e41e0563d8274976b4526021315ea
edgaryungo <eddy8471@gmail.com>
2023-06-12 07:15:19 -0700

    addTuple

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    for i in range(len(my_list)):
+        if mylist[i] % 2 == 0:
+            mylist[i] = True
+        else:
+            mylist[i] = False
+    return my_list
