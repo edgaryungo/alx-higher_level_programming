45cc3814b55dc10b82784a881a4d92405c7913a4
edgaryungo <eddy8471@gmail.com>
2023-06-12 07:17:18 -0700

    addTuple

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    nlist = []
     for i in range(len(my_list)):
         if mylist[i] % 2 == 0:
-            mylist[i] = True
+            nlist.append(True)
         else:
-            mylist[i] = False
-    return my_list
+            nlist.append(False)
+    return nlist
