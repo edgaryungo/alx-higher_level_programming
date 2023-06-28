5c9567be209187881dd3b59ccac29cf4463605c9
edgaryungo <eddy8471@gmail.com>
2023-06-28 09:22:34 -0700

    fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,12 +11,22 @@ class Square:
         Args:
             size (int): size of the square
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
+    @property
+    def size(self):
+        """Gt/set value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value)
+        """Setter mettod for value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """Return area of square"""
         return (self.__size * self.__size)
+
