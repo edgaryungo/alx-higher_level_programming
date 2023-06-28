1a275b7e4a6d21f9c5b337e54a8ec475f6fcd5b3
edgaryungo <eddy8471@gmail.com>
2023-06-28 08:45:50 -0700

    fix

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define a square."""
+
+
+class Square:
+    """Class Square that defines a square."""
+
+    def __init__(self, size=2):
+        """Initialize method that stores the size of the square
+
+        Args:
+            size (int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return area of square"""
+        return self.__size * self.__size
